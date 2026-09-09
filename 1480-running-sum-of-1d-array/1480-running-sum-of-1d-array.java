364d1c798eedcaccf3c75fd48ece6a255488f41e
sahithyatulluru13 <sahithya@myguruv.com>
2026-09-09 22:22:30 +0530

    Time: 0 ms (100%), Space: 44.5 MB (17.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,14 +1,13 @@
-import java.util.Arrays;
 class Solution {
- public int[] runningSum(int[] nums) {
- for (int i = 1; i < nums.length; i++) {
- nums[i] = nums[i] + nums[i - 1];
- }
- return nums;
- }
- public static void main(String[] args) {
- Solution s = new Solution();
- int[] nums = {1, 2, 3, 4}; 
-System.out.println(Arrays.toString(s.runningSum(nums)));
- }
+    public int[] runningSum(int[] nums) {
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            nums[i] = sum;
+        }
+
+        return nums;
+    }
 }
